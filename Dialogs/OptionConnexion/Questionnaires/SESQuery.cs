efa3ffe5d2cb226ce6784680d862447867e7bacc
Melanie Manivanh <melanie.manivanh@gmail.com>
2018-08-14 17:29:34 +0200

    Tentative de fixer la première valeur du promptChoice

--- Dialogs/OptionConnexion/Questionnaires/SESQuery.cs
+++ Dialogs/OptionConnexion/Questionnaires/SESQuery.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -19,6 +20,15 @@ namespace TrevorBot
         private const string notOkay = "Pas d'accord";
         private const string notAllOkay = "Pas du tout  d'accord"; */
 
+        [Prompt("Quel âge as-tu ?")]
+        public string Age { get; set; }
+
+        public enum SexeOption { Masculin, Féminin, Autre }
+        [Prompt("De quel sexe es tu ? {||}")]
+        public SexeOption? Sexe;
+
+        [Prompt("Quel est le plus haut niveau d'éducation que tu aies achevé?")]
+        public string Education { get; set; }
         public enum Food
         {
             [Describe("Yes, I want South indian meal")]
@@ -32,21 +42,27 @@ namespace TrevorBot
             [Describe("Thanks , I dont want any Food")]
             No = 4
         }
-        public enum SatisfactionOption { [Describe("Tout à fait d'accord")] allOkay, [Describe("D'accord")]DAccord, [Describe("Indifférent")]Indifférent, [Describe ("Pas d'accord")] PasDAccord, [Describe("Pas du tout d'accord")] PasDuToutDAccord };
-        public enum SexeOption { Masculin, Féminin, Autre }
+      
+        public enum SatisfactionOption {
+            [Display(Name = "Excellent")]
+            ToutAFAitDAccord =1 ,
+            [Display(Name = "D'accord")]
+            DAccord = 2,
+            [Display(Name = "Indifférent")]
+            Indifferent = 3,
+            [Display(Name = "Pas d'accord")]
+            PasDAccord = 4,
+            [Display(Name = "Pas du tout d'accord")]
+            PasDuToutDAccord =5
+        };
+
         //public List<string> SatisfactionOpt = new List<string>() { allOkay, okay, quiteOkay, notOkay, notAllOkay };
         
         /*
         private void SESFormQuery (IDialogContext context) {
             PromptDialog.Choice(context, null, SatisfactionOpt, "En matière de gestion de la drépanocytose, je pense savoir quels sont les aspects dont je suis insatisfait?", "Cecic n'est pas une option valide", 3);
         }*/
-        [Prompt("Quel âge as-tu ?")]
-        public string Age { get; set; }
-        [Prompt("De quel sexe es tu ? {||}")]
-        public SexeOption? Sexe;
 
-        [Prompt("Quel est le plus haut niveau d'éducation que tu aies achevé?")]
-        public string Education { get; set; }
 
         //private async Task AskQuestionAsync(IDialogContext context) { PromptDialog.Choice(context, null, SatisfactionOpt, "Tout d'abord dis moi ce que tu veux faire", "Cecic n'est pas une option valide", 3);  }
 
